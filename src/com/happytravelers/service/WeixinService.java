8fc7c6fdd9d396dfcb8876013abc155432b823e9
ab409 <409308624@qq.com>
2014-03-26 22:06:43 +0800

    delete files

--- src/com/happytravelers/service/WeixinService.java
+++
@@ -1,9 +0,0 @@
-package com.happytravelers.service;
-
-import com.using.weixin.wxtools.vo.recv.WxRecvMsg;
-import com.using.weixin.wxtools.vo.send.WxSendMsg;
-
-public interface WeixinService
-{
-	WxSendMsg doMsg(WxRecvMsg msg);
-}
